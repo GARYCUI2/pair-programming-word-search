7f1ebe284cf5eba8ff605a08e067cee25ec3b577
CUI <garycui222@gmail.com>
2021-12-01 22:16:30 -0500

    finish

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,36 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const transpose = function(matrix) {
+  let newRow = matrix.length;
+  let newCol = matrix[0].length;
+  let newMatrix = [];
+  for (let i = 0; i < newCol; i++) {
+    newMatrix[i] = [];
+  }
+  //console.log(newMatrix);
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      newMatrix[j][i] = matrix[i][j];
     }
-}
+  }
+  return newMatrix;
+};
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+  //let verticle = transpose(letters);
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  let verticle =  transpose(letters);
+  const verticleJoin = verticle.map(ls => ls.join(''));
+  for (let l of verticleJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+    
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+module.exports = wordSearch;
